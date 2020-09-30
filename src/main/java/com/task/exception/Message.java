f892e83a3934f55e3fd8c50599b019fd71ddb84d
VictoriaZk <vika.zhak1999@gmail.com>
2020-10-01 00:07:39 +0300

    create enum for constant messages

---
+++ src/main/java/com/task/exception/Message.java
@@ -0,0 +1,14 @@
+package com.task.exception;
+
+public enum Message {
+    APOLOGIZE_MESSAGE,
+    HELLO_MESSAGE;
+
+    public String getApologizeMessage() {
+        return "We are sorry, but now we haven't any information about this city. We try to add information later!";
+    }
+
+    public String getHelloMessage() {
+        return "hello! Nice to meet you on this chat. Let's start! Choose the city and i'll give you information about it.";
+    }
+}
